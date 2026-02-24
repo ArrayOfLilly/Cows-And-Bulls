d38dc923626610e1f38816197ccedc94d543beb2
ArrayOfLilly <arrayoflilly@gmail.com>
2026-02-24 19:24:37 +0100

    Initial Commit

---
+++ CowsAndBulls/CowsAndBullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsAndBullsApp.swift
+//  CowsAndBulls
+//
+//  Created by Ildikó Kasza on 2026. 02. 24..
+//
+
+import SwiftUI
+
+@main
+struct CowsAndBullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
